182431edacf461c6c3879496a65c8eb8241f9c8b
layanabushaweesh <layanabushaweesh@gmail.com>
2021-03-30 22:02:04 +0300

    update

--- server.js
+++ server.js
@@ -1,85 +1,104 @@
 'use strict'; 
-
-// this code i weite it after code wrviwe
+// load enviroment variable its the first step
 const dotenv = require('dotenv'); 
 dotenv.config();
-const PORT = process.env.PORT || 3000; 
+// includ app depancies
 const express = require('express'); 
-let app = express(); 
-
+const superAgent =require('superagent')
 const cors = require('cors');
 
-app.use(cors()); 
+//set up our app
+let app = express();
 
+//set up envitoment variables
+const PORT = process.env.PORT || 3000; 
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const PARKS_API_KEY = process.env.PARKS_API_KEY;
+
+//set our application midlewear 
+app.use(cors()); 
+//our route middleweard
 app.get('/location', handleLocation); 
 app.get('/weather', handleWeather); 
-
-app.get('*', handleErrors); 
-
-
-
-function handleLocation(req, res) {
-    try {
-        let srchQ = req.query.city; 
-        let locationObj = locationData(srchQ); 
-        res.status(200).json(locationObj);
-    } catch (error) {
-        res.status(500).send(`wrong ${error}`);
-    }
-}
-
-function handleWeather(req, res) {
-    let searchQuery = req.query.city;
-    let wthrObj = dataWether(searchQuery);
-    try {
-        res.status(200).send(wthrObj);
-    } catch (error) {
-        res.status(500).send(` wrong ${error}`);
-    }
-}
-function handleErrors(req, res) {
-    res
-        .status(404)
-        .send({ status: 404, responseText: 'Sorry not exist' });
-}
-
-function CityLocation(srchQ, dsplyNam, lat, long) {
-    this.search_query = srchQ;
-    this.formatted_query = dsplyNam;
-    this.latitude = lat;
-    this.longitude = long;
-}
-
-function CityWeather(srchQ, wthrDesc, time) {
-    this.search_query = srchQ;
-    this.forecast = wthrDesc;
-    this.time = time;
-}
-
-
-function dataWether(searchQuery) {
-    let wthrData = require('./data/weather.json');
-    let wthrArry = [];
-    for (let i = 0; i < wthrData.data.length; i++) {
-        let weatherDesc = wthrData.data[i].weather['description'];
-       
-    
-        let resObj = new CityWeather(searchQuery, weatherDesc, newDate);
-        wthrArry.push(resObj);
-    }
-    return wthrArry;
-}
-
-function locationData(searchQuery) {
-   
-    let locationData = require('./data/location.json');
-    let displayName = locationData[0].display_name;
-    let latitude = locationData[0].lat;
-    let longitude = locationData[0].lon;
-    let resObj = new CityLocation(searchQuery, displayName, latitude, longitude);
-    return resObj;
-}
+app.get('/parks', handlePark); 
+app.use('*',handleError)
 app.listen(PORT, () => {
-    
-    console.log(`the app is listening to ${PORT}`); /
+ console.log(`the app is listen to ${PORT}`); 
 })
+
+//FUNction for each path
+function handleLocation(req, res) {
+    const url = 'https://us1.locationiq.com/v1/search.php?key=' + GEOCODE_API_KEY + '&q=' + req.query.city + '&format=json&limit=1';
+    superAgent.get(url).then(locationData => {
+        const data = locationData.body[0];
+        res.status(200).json(new Location(req.query.city, data.display_name, data.lat, data.lon));
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send('wrong');
+    })
+   
+}
+
+async function handleWeather(req, res) {
+    try {
+        const city = req.query.search_query;
+
+        const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${WEATHER_API_KEY}`;
+        const rawWeatherData = await superAgent.get(url);
+        const weatherData = JSON.parse(rawWeatherData.text).data;
+
+        const forecasts = weatherData.map(element => {
+            const description = element.weather.description;
+            const time = element.datetime;
+            return new Weather(description, time);
+        });
+        res.json(forecasts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(' wrong');
+    }
+   
+}
+function handlePark (req, res) {
+    const url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${PARKS_API_KEY}`;
+    superAgent.get(url).then(parksData => {
+        const parks = parksData.body.data.map(data => {
+            const name = data.fullName;
+            const address = data.addresses[0].line1 + data.addresses[0].city;
+            const sun = data.entranceFees[0].cost;
+            const description = data.description;
+            const parkURL = data.url;
+            return new Park(name, address, sun, description, parkURL)
+        });
+        res.status(200).json(parks);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send(' wrong');
+    })
+}
+
+function handleError (req,res) {
+    console.log(req)
+    res.status(404).send(`cant get ${req.baseUrl}`)
+} 
+
+
+function Location(name, location, latitude, longitude) {
+    this.search_query = name;
+    this.formatted_query = location;
+    this.latitude = latitude;
+    this.longitude = longitude;
+}
+
+function Park(name, address, sun, description, url) {
+    this.name = name;
+    this.address = address;
+    this.sun = sun;
+    this.description = description;
+    this.url = url;
+}
+function Weather(description, valid_date) {
+    this.forecast = description;
+    this.time = valid_date;
+}
